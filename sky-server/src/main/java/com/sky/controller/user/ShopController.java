390703472b3ef0e6149f2cb0335b35270ba1c322
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2025-08-01 07:41:03 +0800

    feat: 用户端获取店铺营业状态

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,42 @@
+package com.sky.controller.user;
+
+import com.sky.constant.MessageConstant;
+import com.sky.exception.BaseException;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    @Qualifier("myRedisTemplate")// 指定使用myRedisTemplate
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 获取营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result getStatus(){
+        log.info("获取营业状态");
+
+        String status = (String) redisTemplate.opsForValue().get(KEY);
+
+        if(status == null)
+            throw new BaseException(MessageConstant.SHOP_STATUS_ERROR);
+
+        return Result.success(Integer.parseInt(status));
+    }
+}
+
